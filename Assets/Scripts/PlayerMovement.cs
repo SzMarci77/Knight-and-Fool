6b17c933d053c99ef4b49208652beecb4fd57e34
Marcell Szabó <marcellszabo77@gmail.com>
2026-03-30 22:13:24 +0200

    Bugfix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
